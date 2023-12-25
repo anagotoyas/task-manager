072f8666340889c0ae1134ffb5224a987b325ed6
anagotoyas <ana20otoya@gmail.com>
2023-12-25 16:18:44 -0500

    fix issues

--- src/components/TaskCell/TaskEmpty/index.tsx
+++ src/components/TaskCell/TaskEmpty/index.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import styled from 'styled-components';
 import { useGlobal } from '../../../context/GlobalContext';
 
--- src/components/TaskCell/TaskHeader/index.tsx
+++ src/components/TaskCell/TaskHeader/index.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { useGlobal } from '../../../context/GlobalContext'
 import styled from 'styled-components'
 import { RiArrowDownSFill } from 'react-icons/ri'
